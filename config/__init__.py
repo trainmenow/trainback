99d9bf25e05a31d31c1516f52f07eb312138a5df
Maximilian Kromer <a.makro@web.de>
2019-11-18 20:39:44 +0100

    initialize flask project

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+
+config_by_name = dict(
+    dev='config.development',
+    test='config.testing',
+    prod='config.production',
+    stage='config.staging'
+)
---
+++ manage.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from trainback import create_app, db
+
+app = create_app(os.getenv('APP_CONFIG') or 'dev')
+
+app.app_context().push()
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def run():
+    app.run()
+
+
+@manager.command
+def test():
+    """Runs the unit tests."""
+
+    # Code for unittesting
+
+
+if __name__ == '__main__':
+    manager.run()
