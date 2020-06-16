6ab19c19a1e8ecb9a1bffceef41c73a75600eecb
Maximilian Kromer <maximiliankromer@MaximiliansiPro.fritz.box>
2020-06-16 19:23:06 +0200

    added trainlog / started trackme

--- trainback/trainlog/__init__.py
+++ trainback/trainlog/__init__.py
@@ -1,12 +1,37 @@
 from flask import Blueprint
 from flask_restplus import Api
 
+from trainback.trainlog.workout.controller import api as workout_ns
+
 blueprint = Blueprint('logger_api', __name__, url_prefix='/log')
+
+authorizations = {
+    '-- How to Authorize --': {
+        'type': 'apiKey',
+        'name': 'Write the following in the field:',
+        'in': '"Bearer + [yourJWT-Token]"'
+    },
+    'jwt': {
+        'type': 'apiKey',
+        'name': 'Authorization',
+        'in': 'header'
+    },
+    'jwt-refresh': {
+        'type': 'apiKey',
+        'name': 'Authorization',
+        'in': 'header'
+    },
+    'admin': {
+        'type': 'apiKey',
+        'name': 'Authorization',
+        'in': 'header'
+    }}
 
 api = Api(blueprint,
           title='TRAINBACK LOGGER API',
           version='0.1',
-          description='an api logging training sessions and foofplans'
+          description='an api logging training sessions and foodplans',
+          authorizations=authorizations
           )
 
-# api.add_namespace()
+api.add_namespace(workout_ns, path='/workout')
---
+++ trainback/trainlog/workout/controller.py
@@ -0,0 +1,38 @@
+from flask import request
+from flask_restplus import Resource
+
+from trainback.trainlog.workout import api
+
+from trainback.trainmanager.auth.decorators import admin_required, user_required
+
+
+@api.route('/')
+class Workouts(Resource):
+    @api.doc('list of all workouts', security='jwt')
+    @user_required
+    def get(self):
+        pass
+    
+    @api.doc('create a new workout', security='jwt')
+    @user_required
+    def post(self):
+        pass
+
+
+@api.route('/<workout_id>')
+@api.param('workout_id', 'The workout identifier')
+class SingleWorkout(Resource):
+    @api.doc('detailed information of one workout', security='jwt')
+    @user_required
+    def get(self):
+        pass
+    
+    @api.doc('update a workout', security='jwt')
+    @user_required
+    def put(self):
+        pass
+    
+    @api.doc('delete a workout', security='jwt')
+    @user_required
+    def delete(self):
+        pass
