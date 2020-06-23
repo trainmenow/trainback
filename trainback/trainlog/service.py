0b7217a790e260eb0ca03875c67e5c5bac2e4522
Maximilian Kromer <a.makro@web.de>
2020-06-23 23:28:23 +0200

    added workout creating endpoint

--- trainback/trainlog/service.py
+++ trainback/trainlog/service.py
@@ -6,3 +6,8 @@ def get_exercises():
 def create_exercise(data):
     exercise_id = mongo.db.exercises.insert_one(data).inserted_id
     return str(exercise_id), 201
+
+def create_workout(data, pub_id):
+    data['user'] = pub_id
+    workout_id = mongo.db.workouts.insert_one(data).inserted_id
+    return str(workout_id), 201
--- trainback/trainlog/workout/controller.py
+++ trainback/trainlog/workout/controller.py
@@ -1,7 +1,11 @@
 from flask import request
 from flask_restx import Resource
+# from bson.objectid import ObjectId
+from flask_jwt_extended import get_jwt_identity
 
 from trainback.trainlog.workout import api
+from trainback.trainlog.service import create_workout
+from trainback.trainlog.workout.parser import workout_parser
 
 from trainback.trainmanager.auth.decorators import admin_required, user_required
 
@@ -15,24 +19,26 @@ class Workouts(Resource):
     
     @api.doc('create a new workout', security='jwt')
     @user_required
+    @api.expect(workout_parser)
     def post(self):
-        pass
+        data = workout_parser.parse_args(request)
+        return create_workout(data.workout, get_jwt_identity())
 
 
-@api.route('/<workout_id>')
+@api.route('/<ObjectId:workout_id>')
 @api.param('workout_id', 'The workout identifier')
 class SingleWorkout(Resource):
     @api.doc('detailed information of one workout', security='jwt')
     @user_required
-    def get(self):
+    def get(self, workout_id):
         pass
     
     @api.doc('update a workout', security='jwt')
     @user_required
-    def put(self):
+    def patch(self, workout_id):
         pass
     
     @api.doc('delete a workout', security='jwt')
     @user_required
-    def delete(self):
+    def delete(self, workout_id):
         pass
